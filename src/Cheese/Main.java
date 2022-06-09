cbb4a0f42226dfa09335198d9959b72608d274af
Curro Calderón <xenomorfo270@gmail.com>
2022-06-09 18:43:20 +0200

    COMMIT master MAIN

--- src/Cheese/Main.java
+++ src/Cheese/Main.java
@@ -17,6 +17,18 @@ public class Main {
 	 * @author Francisco Javier Calderón Aragón
 	 */
      public static void main(String[] args) {
+    	 Producto p1,p2,p3;
+    	 ListaProductos Lista = new ListaProductos("curro");
+    	 Producto producto1 = new Producto("0001","lácteo",2,100);
+    	 Producto producto2 = new Producto("0002","verdura",1,200);
+    	 
+    	 Lista.registraProducto(producto1);
+    	 Lista.registraProducto(producto2);
+    	 producto1.imprime_etiqueta();
+    	 producto2.imprime_etiqueta();
+    	 
+    	
+    	 
     
          System.out.println("1Âº Gdam: Entornos de desarrollo");
 
